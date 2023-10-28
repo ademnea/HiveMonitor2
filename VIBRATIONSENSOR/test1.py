eac7aef9dcb4350be545ffeb26c18236d781cf61
KigwanaAugustine <augustinekigz45@gmail.com>
2023-10-28 12:27:35 +0300

    vibration Sensor code3

--- VIBRATIONSENSOR/test1.py
+++ VIBRATIONSENSOR/test1.py
@@ -38,6 +38,17 @@ else:
   # The sensor has not been moved
   print("The sensor has not been moved.")
 
+i2c = board.I2C()  # uses board.SCL and board.SDA
+# i2c = board.STEMMA_I2C()  # For using the built-in STEMMA QT connector on a microcontroller
+
+# For ADXL343
+#accelerometer = adafruit_adxl34x.ADXL343(i2c)
+# For ADXL345
+accelerometer = adafruit_adxl34x.ADXL345(i2c)
+
+accelerometer.enable_tap_detection()
+# you can also configure the tap detection parameters when you enable tap detection:
+# accelerometer.enable_tap_detection(tap_count=2,threshold=20, duration=50)
 
 # Save the initial coordinates
 save_coordinates(accelerometer.acceleration[0], accelerometer.acceleration[1], accelerometer.acceleration[2])
