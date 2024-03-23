a7fa90fd7d06b6341304342f6f485e95e5760261
Shawal Mbalire <mbalireshawal@gmail.com>
2024-03-23 11:31:24 +0300

    edited mmcature

--- multimedia_capture/config.py
+++ multimedia_capture/config.py
@@ -1,12 +1,12 @@
 # client configurations
-
+import os
 # This is an auto-generated file
 # You can edit these configurations directly or by running the setup.py script
 # Note that errors in the config file can result into the failure of the whole client
 # So be careful while editing, thanks 
 
 from datetime import datetime
-from ../config import base_dir
+base_dir = os.pwd()
 
 node_id = 2
 username = "agatha"
